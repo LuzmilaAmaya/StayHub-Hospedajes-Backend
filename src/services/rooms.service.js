d4e21142028393b44d1776cd6dcc0d036f3d7a41
luzmila amaya <amayaluzmila661@gmail.com>
2026-02-15 17:52:48 -0300

    se arrgelo problema en room service

--- src/services/rooms.service.js
+++ src/services/rooms.service.js
@@ -1,5 +1,22 @@
-import axios from "axios";
+import Room from "../models/Room.js";
 
-export const getRooms = () => {
-  return axios.get("/api/rooms");
+export const createRoom = async (data) => {
+  const newRoom = new Room(data);
+  return await newRoom.save();
+};
+
+export const getAllRooms = async () => {
+  return await Room.find();
+};
+
+export const getRoomById = async (id) => {
+  return await Room.findById(id);
+};
+
+export const updateRoom = async (id, data) => {
+  return await Room.findByIdAndUpdate(id, data, { new: true });
+};
+
+export const deleteRoom = async (id) => {
+  return await Room.findByIdAndDelete(id);
 };

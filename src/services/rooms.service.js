7d3960fe1f839dbae3c835f3800f3bd6138ef922
luzmila amaya <amayaluzmila661@gmail.com>
2026-02-15 17:33:55 -0300

    se modifico el archivo room services

--- src/services/rooms.service.js
+++ src/services/rooms.service.js
@@ -1,21 +1,5 @@
-import Room from "../models/Room.js";
+import axios from "axios";
 
-export const createRoom = (data) => {
-  return Room.create(data);
-};
-
-export const getAllRooms = () => {
-  return Room.find();
-};
-
-export const getRoomById = (id) => {
-  return Room.findById(id);
-};
-
-export const updateRoom = (id, data) => {
-  return Room.findByIdAndUpdate(id, data, { new: true });
-};
-
-export const deleteRoom = (id) => {
-  return Room.findByIdAndDelete(id);
+export const getRooms = () => {
+  return axios.get("/api/rooms");
 };

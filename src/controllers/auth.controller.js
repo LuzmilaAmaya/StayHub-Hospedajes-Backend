2edc08af80f33d8e9595a134e0c15d8af7afdf1c
luzmila amaya <amayaluzmila661@gmail.com>
2026-04-08 17:05:37 -0300

    se  agrego funcionalidad de crear tokens y se pueden guardar los usuarios en la base d datos 2

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 import { registerService, loginService } from "../services/auth.service.js";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
@@ -16,5 +18,39 @@ export const login = async (req, res) => {
     res.json(result);
   } catch (error) {
     res.status(401).json({ message: error.message });
+  }
+};
+export const googleAuth = async (req, res) => {
+  try {
+    console.log("BODY:", req.body);
+    const { fullName, email, googleId, photo } = req.body;
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({
+        fullName,
+        email,
+        googleId,
+        password: null,
+        phone: null,
+        documentId: null,
+      });
+    } else {
+      user.googleId = googleId || user.googleId;
+      user.fullName = fullName || user.fullName;
+
+      await user.save();
+    }
+    console.log("JWT:", process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "7d" },
+    );
+
+    res.json({ user, token });
+   } catch (error) {
+    console.log(" ERROR GOOGLE BACKEND:", error); 
+    res.status(500).json({ message: error.message });
   }
 };

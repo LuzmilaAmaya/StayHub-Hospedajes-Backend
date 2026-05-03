7065443e4ad7e5e886f80f840f0497605accd55d
luzmila amaya <amayaluzmila661@gmail.com>
2026-05-03 02:00:38 -0300

    se creo el archivo api index.js para conectar backend al front

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export const connectDB = async () => {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-   console.log("Base conectada:", mongoose.connection.name);
+    const db = await mongoose.connect(process.env.MONGO_URI);
+    isConnected = db.connections[0].readyState;
+    console.log("Base conectada:", mongoose.connection.name);
   } catch (error) {
     console.error("Error conectando MongoDB", error);
-    process.exit(1);
   }
 };

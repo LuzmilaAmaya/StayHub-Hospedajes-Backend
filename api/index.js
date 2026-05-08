b1c97eeba66baea10ed9a8dc2fb5005792a60e00
luzmila amaya <amayaluzmila661@gmail.com>
2026-05-07 22:07:38 -0300

    test minimal vercel

--- api/index.js
+++ api/index.js
@@ -1,33 +1,9 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-
-import routes from "../src/routes/index.js";
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-  ],
-  credentials: true,
-}));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", routes);
-
-// ruta prueba
 app.get("/", (req, res) => {
-  res.send("API Hotel funcionando 🚀");
+  res.send("Backend funcionando 🚀");
 });
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB conectado ✅"))
-  .catch((error) => console.error("Error MongoDB ❌", error));
 
 export default app;
